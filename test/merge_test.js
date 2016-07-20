681a18db4353ee5c36804254e832a71b3a719b33
Otávio Augusto Soares <otaviokk@gmail.com>
2016-07-19 23:26:04 -0300

    add changelog

---
+++ test/merge_test.js
@@ -0,0 +1,100 @@
+'use strict'
+
+const Seneca = require('seneca')
+const Stub = require('seneca-stub')
+const Entity = require('seneca-entity')
+const EventSourcing = require('..')
+const test = require('tape')
+
+function SenecaInstance () {
+  var seneca = Seneca({
+    log: 'silent',
+    default_plugins: {
+      entity: false,
+      'mem-store': false
+    },
+    plugins: [Entity, EventSourcing]
+  })
+
+  return seneca
+}
+
+function TestEntity (si) {
+  var entity = si.make_sourced('test')
+  entity.property = false
+  entity.property2 = {
+    subProperty: false,
+    subProperty2: true
+  }
+  return entity
+}
+
+test('should throw an entity error with name of model when attempting to replay a pattern an entity not handled', function (t) {
+  var si = SenecaInstance()
+  var entity = si.make_sourced('test')
+
+  var events = [{
+    command: 'some-command',
+    data: { some: 'param' }
+  }]
+  entity.replay(events, function (err) {
+    t.true(/No matching action pattern found for/.test(err))
+
+    t.end()
+    si.close()
+  })
+})
+
+test('should merge a snapshot into the current snapshot, overwriting any common properties', function (t) {
+  var si = SenecaInstance()
+  var entity = TestEntity(si)
+
+  var snapshot = {
+    property: true,
+    property2: true
+  }
+
+  entity.merge(snapshot)
+
+  t.true(entity.property)
+  t.true(entity.property2)
+  t.end()
+  si.close()
+})
+
+test('should merge a complex snapshot (missing newly added fields) while maintaining defaulted sub-object values', function (t) {
+  var si = SenecaInstance()
+  var entity = TestEntity(si)
+
+  var snapshot = {
+    property: true
+  }
+
+  entity.merge(snapshot)
+
+  t.true(entity.property)
+  t.false(entity.property2.subProperty)
+  t.true(entity.property2.subProperty2)
+  t.end()
+  si.close()
+})
+
+test('should merge a complex snapshot while maintaining defaulted sub-object values', function (t) {
+  var si = SenecaInstance()
+  var entity = TestEntity(si)
+
+  var snapshot = {
+    property: true,
+    property2: {
+      subProperty: true,
+      subProperty2: false
+    }
+  }
+  entity.merge(snapshot)
+
+  t.true(entity.property)
+  t.true(entity.property2.subProperty)
+  t.false(entity.property2.subProperty2)
+  t.end()
+  si.close()
+})
