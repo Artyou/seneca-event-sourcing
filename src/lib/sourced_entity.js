24025b3c3b7f1d9dacaefe5aba6549b2ac5999f2
Otavio Augusto <otavio@machobear.ca>
2019-04-03 21:24:22 -0700

    Upgrade sourced

--- src/lib/sourced_entity.js
+++ src/lib/sourced_entity.js
@@ -1,25 +1,21 @@
 'use strict'
 
-var Util = require('util')
-var Entity = require('sourced').Entity
+var Entity = require('sourced').SourcedEntity
 
 var Common = require('./common')
 
-function SourcedEntity () {
-  Entity.apply(this, arguments)
-}
-Util.inherits(SourcedEntity, Entity)
+class SourcedEntity extends Entity {
+  emit() {
+    if (!this.replaying) {
+      var args = Common.arrayify(arguments)
+      var pattern = {role: 'events', fatal$: false}
+      var event = args.shift()
+      if (event.indexOf('.') === -1) throw Error('Events should be queued in object.event format.')
 
-SourcedEntity.prototype.emit = function emit () {
-  if (!this.replaying) {
-    var args = Common.arrayify(arguments)
-    var pattern = {role: 'events', fatal$: false}
-    var event = args.shift()
-    if (event.indexOf('.') === -1) throw Error('Events should be queued in object.event format.')
-
-    var split = event.split('.')
-    pattern[split[0]] = split[1]
-    this.private$.seneca.act(pattern, {data: args})
+      var split = event.split('.')
+      pattern[split[0]] = split[1]
+      this.private$.seneca.act(pattern, {data: args})
+    }
   }
 }
 
